3b1dfe47b6bb4ed671dd64d8aeb4f1d1c1b29db4
brayoh <bkuriakb@gmail.com>
2017-08-30 18:00:43 +0300

    [FEATURE] Add api service for authentication requests

---
+++ src/common/authService.js
@@ -0,0 +1,47 @@
+import instance from "./axios_config";
+
+function loginUser(user) {
+  return instance.request({
+    url: '/auth/login',
+    method: 'POST',
+    data: {
+      username: user.username,
+      password: user.password
+    }
+  }).then(function(response) {
+    return response.data
+  })
+}
+
+function registerUser(user) {
+  return instance.request({
+    url: '/auth/register',
+    method: 'POST',
+    data: {
+      username: user.username,
+      password: user.password
+    }
+  }).then(function(response) {
+    return response.data
+  })
+}
+
+function fetchUserData(token) {
+  return instance.request({
+    url: '/whoami',
+    method: 'GET',
+    headers: {
+      "Authorization":  token
+    }
+  }).then(function(response) {
+    return response.data
+  })
+}
+
+const Api = Object.assign({}, {
+  loginUser,
+  registerUser,
+  fetchUserData
+})
+
+export default Api;
