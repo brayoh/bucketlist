b40ebc5953353467095236fd276d020fcd239182
brayoh <bkuriakb@gmail.com>
2017-08-30 16:08:46 +0300

    [CHORE] Update axios config request headers

--- src/common/axios_config.js
+++ src/common/axios_config.js
@@ -4,7 +4,9 @@ const instance = axios.create({
   // baseURL: 'https://awesome-bucketlist-api.herokuapp.com/api/v1/',
   baseURL: 'http://127.0.0.1:5000/api/v1/',
   timeout: 10000,
-  headers: new Headers({'Content-Type': 'application/json'})
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default instance;
