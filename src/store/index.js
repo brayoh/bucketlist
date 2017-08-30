1afddc8d6476181118bf1a0305956edcb23a03e3
brayoh <bkuriakb@gmail.com>
2017-08-30 17:59:05 +0300

    [FEATURE] Add store config

--- src/store/index.js
+++ src/store/index.js
@@ -7,19 +7,29 @@ import logger from 'redux-logger';
 import * as reducers from "../reducers";
 import rootSaga from "../sagas";
 import initialState from "../reducers/defaultState";
+import { whoami, loginUserSuccess } from "../actions/authorization";
 
 const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore (){
+  console.log(reducers)
+  const store = createStore(combineReducers({
+      ...reducers,
+      "routing": routerReducer
+    }), applyMiddleware(sagaMiddleware, logger))
 
-  const store = createStore(combineReducers({ ...reducers,
-                                              "routing": routerReducer
-                                            }),
-                                            applyMiddleware(sagaMiddleware, logger))
-     console.log(store.getState())
     const history = syncHistoryWithStore(browserHistory, store)
 
     sagaMiddleware.run(rootSaga)
 
-    return {store: store, history: history};
+    let token = localStorage.getItem('awesome_bucketlist_token')
+
+    if (token === null) {
+        token = sessionStorage.getItem('token')
+    }
+    if (token !== null) {
+        store.dispatch(loginUserSuccess());
+        store.dispatch(whoami(token));
+    }
+    return {store, history};
 }
