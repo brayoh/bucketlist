4eff67cc47012006ff107042f240d38de2799c22
brayoh <bkuriakb@gmail.com>
2017-08-30 16:07:44 +0300

    [CHORE] Add constant for action types

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,11 +1,47 @@
 // @flow
 
 // login
-export const LOGIN_REQUEST: string = "LOGIN_REQUEST";
-export const LOGIN_FAILURE: string = "LOGIN_FAILURE";
-export const LOGIN_SUCCESS: string = "LOGIN_SUCCESS";
+export const LOGIN_REQUEST: string = 'LOGIN_REQUEST';
+export const LOGIN_FAILURE: string = 'LOGIN_FAILURE';
+export const LOGIN_SUCCESS: string = 'LOGIN_SUCCESS';
 
 // sign up
-export const SIGNUP_REQUEST: string = "SIGNUP_REQUEST";
-export const SIGNUP_FAILURE: string = "SIGNUP_FAILURE";
-export const SIGNUP_SUCCESS: string = "SIGNUP_SUCCESS";
+export const SIGNUP_REQUEST: string = 'SIGNUP_REQUEST';
+export const SIGNUP_FAILURE: string = 'SIGNUP_FAILURE';
+export const SIGNUP_SUCCESS: string = 'SIGNUP_SUCCESS';
+
+// whoami check
+export const WHOAMI_REQUEST: string = 'WHOAMI_REQUEST';
+export const WHOAMI_SUCCESS: string = 'WHOAMI_SUCCESS';
+export const WHOAMI_FAILURE: string = 'WHOAMI_FAILURE';
+
+
+// get bucketlists
+export const GET_BUCKETLISTS_REQUEST: string = 'GET_BUCKETLISTS_REQUEST';
+export const GET_BUCKETLISTS_SUCCESS: string = 'GET_BUCKETLISTS_SUCCESS';
+export const GET_BUCKETLISTS_FAILURE: string = 'GET_BUCKETLISTS_FAILURE';
+
+// update a bucketlist
+export const UPDATE_BUCKETLIST_REQUEST: string = 'UPDATE_BUCKETLIST_REQUEST';
+export const UPDATE_BUCKETLIST_SUCCESS: string = 'UPDATE_BUCKETLIST_SUCCESS';
+export const UPDATE_BUCKETLIST_FAILURE: string = 'UPDATE_BUCKETLIST_FAILURE';
+
+// delete a bucketlist
+export const DELETE_BUCKETLIST_REQUEST: string = 'DELETE_BUCKETLIST_REQUEST';
+export const DELETE_BUCKETLIST_SUCCESS: string = 'DELETE_BUCKETLIST_SUCCESS';
+export const DELETE_BUCKETLIST_FAILURE: string = 'DELETE_BUCKETLIST_FAILURE';
+
+// // get bucketlists items
+// export const GET_ITEMS_REQUEST: string = 'GET_ITEMS_REQUEST';
+// export const GET_ITEMS_SUCCESS: string = 'GET_ITEMS_SUCCESS';
+// export const GET_ITEMS_FAILURE: string = 'GET_ITEMS_FAILURE';
+
+// update bucketlist item
+export const UPDATE_ITEM_REQUEST: string = 'UPDATE_ITEM_REQUEST';
+export const UPDATE_ITEM_SUCCESS: string = 'UPDATE_ITEM_SUCCESS';
+export const UPDATE_ITEM_FAILURE: string = 'UPDATE_ITEM_FAILURE';
+
+// delete bucketlist item
+export const DELETE_ITEM_REQUEST: string = 'DELETE_ITEM_REQUEST';
+export const DELETE_ITEM_SUCCESS: string = 'DELETE_ITEM_SUCCESS';
+export const DELETE_ITEM_FAILURE: string = 'DELETE_ITEM_FAILURE';
