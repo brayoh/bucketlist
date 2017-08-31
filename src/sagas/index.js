65a59038f743bcd0ea124385bc4538fc6488898f
brayoh <bkuriakb@gmail.com>
2017-08-31 06:19:36 +0300

    [CHORE] Add create and delete bucketlist sagas to root saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,13 +1,15 @@
 import { takeLatest } from 'redux-saga/effects';
 import {loginUser, registerUser} from "./authorizationSagas";
-import {getBucketlists} from "./bucketlistSagas";
+import {getBucketlists, addBucketlist, deleteBucketlist} from "./bucketlistSagas";
 import {whoami} from "./whoamiSaga";
 
 import {
   LOGIN_REQUEST,
   SIGNUP_REQUEST,
   WHOAMI_REQUEST,
-  GET_BUCKETLISTS_REQUEST
+  GET_BUCKETLISTS_REQUEST,
+  ADD_BUCKETLIST_REQUEST,
+  DELETE_BUCKETLIST_REQUEST
 } from "../actions/constants";
 
 // single entry point to start all Sagas at once
@@ -16,6 +18,8 @@ export default function* rootSaga() {
     takeLatest(LOGIN_REQUEST, loginUser),
     takeLatest(SIGNUP_REQUEST, registerUser),
     takeLatest(GET_BUCKETLISTS_REQUEST, getBucketlists),
+    takeLatest(ADD_BUCKETLIST_REQUEST, addBucketlist),
+    takeLatest(DELETE_BUCKETLIST_REQUEST, deleteBucketlist),
     takeLatest(WHOAMI_REQUEST, whoami)
   ]
 }
