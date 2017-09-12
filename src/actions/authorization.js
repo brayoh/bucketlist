e9e6c1e9b2fadf52e4c5d3e90b60396ed89c153b
brayoh <bkuriakb@gmail.com>
2017-09-12 15:33:31 +0300

    [CHORE] Add logout action

--- src/actions/authorization.js
+++ src/actions/authorization.js
@@ -1,10 +1,19 @@
-import {SIGNUP_REQUEST} from "./constants";
-import {LOGIN_REQUEST, LOGIN_SUCCESS, WHOAMI_REQUEST} from "./constants";
+import {
+  SIGNUP_REQUEST,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  WHOAMI_REQUEST,
+  REDIRECT,
+  LOGOUT_USER
+} from "./constants";
 
-export const login: Object = (user) => ({type: LOGIN_REQUEST, user})
 
-export const loginUserSuccess: Object = () => ({type: LOGIN_SUCCESS})
+export const login : Object = (user) => ({ type: LOGIN_REQUEST, user })
 
-export const signup: Object = (user) => ({type: SIGNUP_REQUEST, user})
+export const logoutUser : Object = () => ({ type: LOGOUT_USER })
 
-export const whoami: Object = (token) => ({type: WHOAMI_REQUEST, token})
+export const loginUserSuccess : Object = () => ({ type: LOGIN_SUCCESS })
+
+export const signup : Object = (user) => ({ type: SIGNUP_REQUEST, user })
+
+export const whoami : Object = (token) => ({ type: WHOAMI_REQUEST, token })
